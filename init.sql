1db6852e10739a626e2ee36494556274f8e69581
gnguilherme <ggaighernt@gmail.com>
2024-09-30 11:53:31 -0300

    Refactor database schema to use INT type for user_id in messages table

--- init.sql
+++ init.sql
@@ -1,19 +1,19 @@
 CREATE TABLE IF NOT EXISTS users (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     username VARCHAR(50) NOT NULL UNIQUE
 );
 
 CREATE TABLE IF NOT EXISTS chats (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     user_id INT,
     chat_id TEXT,  -- This is a hash of the user_id, the chat_id, and the timestamp
     FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
 CREATE TABLE IF NOT EXISTS messages (
-    id INT SERIAL PRIMARY KEY,
+    id SERIAL PRIMARY KEY,
     chat_id INT,
-    user_id TEXT,
+    user_id INT,
     message TEXT,
     timestamp TIMESTAMP,
     FOREIGN KEY (chat_id) REFERENCES chats(id),
