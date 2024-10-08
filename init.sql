7742063b6c3450b27c823b2ea45360d5be40b14e
gnguilherme <ggaighernt@gmail.com>
2024-10-08 11:00:36 -0300

    feat: Create blockchain table in init.sql
    * Create a new table called "blockchain" in the `init.sql` file to store blocks of data.

--- init.sql
+++ init.sql
@@ -1,21 +1,4 @@
-CREATE TABLE IF NOT EXISTS users (
+CREATE TABLE IF NOT EXISTS blockchain (
     id SERIAL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL UNIQUE
-);
-
-CREATE TABLE IF NOT EXISTS chats (
-    id SERIAL PRIMARY KEY,
-    user_id INT,
-    chat_id TEXT,  -- This is a hash of the user_id, the chat_id, and the timestamp
-    FOREIGN KEY (user_id) REFERENCES users(id)
-);
-
-CREATE TABLE IF NOT EXISTS messages (
-    id SERIAL PRIMARY KEY,
-    chat_id INT,
-    user_id INT,
-    message TEXT,
-    timestamp TIMESTAMP,
-    FOREIGN KEY (chat_id) REFERENCES chats(id),
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    blocks TEXT
 );
